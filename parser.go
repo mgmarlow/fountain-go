761b96d28a403dd1067edeff1b31ed21f1c1977f
Graham Marlow <mgmarlow@hey.com>
2022-07-31 08:56:31 -0700

    wip

--- parser.go
+++ parser.go
@@ -1,2 +1,63 @@
 package main
 
+import (
+	"fmt"
+)
+
+// Influenced by mdast:
+// https://github.com/remarkjs/remark#syntax-tree
+
+//
+// Node Overview
+//
+// FlowContent: Sections of a screenplay
+// Transition | Section | SceneHeading | PageBreak | Lyric | Character | Note | Boneyard
+//
+// Content: Text that forms paragraphs
+// Dialogue | Action | Parenthetical
+//
+// PhrasingContent: Text in a document & its markup
+// Emphasis | Underline | Strong | Text
+//
+
+type node struct {
+	kind     string
+	value    string
+	children []node
+}
+
+type parser struct {
+	current int
+	tokens  []Token
+}
+
+func Parse(tokens []Token) node {
+	p := NewParser(tokens)
+	ast := node{
+		kind:     "Root",
+		children: []node{},
+	}
+
+	for p.current < len(p.tokens) {
+		ast.children = append(ast.children, p.walk())
+	}
+
+	return ast
+}
+
+func NewParser(tokens []Token) *parser {
+	return &parser{
+		current: 0,
+		tokens:  tokens,
+	}
+}
+
+func (p *parser) walk() node {
+	token := p.tokens[p.current]
+
+	fmt.Printf("%s: %s\n", token.kind, token.value)
+
+	p.current++
+
+	return node{}
+}
--- parser_test.go
+++ parser_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestWalkNesting(t *testing.T) {
+	input := []Token{
+		{"text", "From what seems like only INCHES AWAY. "},
+		{"underscore", "_"},
+		{"text", "Steel's face FILLS the "},
+		{"asterisk", "*"},
+		{"text", "Leupold Mark 4"},
+		{"asterisk", "*"},
+		{"text", " scope"},
+		{"underscore", "_"},
+		{"text", "."},
+	}
+	got := Parse(input)
+
+	wanted := node{}
+
+	if !reflect.DeepEqual(got, wanted) {
+		t.Error("\nExpected:", wanted, "\nGot:", got)
+	}
+}
